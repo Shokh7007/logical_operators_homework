1fea8486636b3e771e7d18699ee6dddd6e9e41db
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-18 23:01:09 +0500

    17

--- logic17.py
+++ logic17.py
@@ -6,4 +6,10 @@ def main(a):
     Returns:
         bool: answer
     """
-    return
+    b=a%100000//10000
+    c=a%10000//1000
+    d=a%1000//100
+    e=a%100//10
+    f=a%10
+    return b<c<d<e<f
+print(main(12645))
