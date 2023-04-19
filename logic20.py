1f1247916a8b8aa0b894b71b246825ab6b905c9c
Shokh7007 <shoxruxbeknafasov9@gmail.com>
2023-04-19 13:40:42 +0500

    20

--- logic20.py
+++ logic20.py
@@ -8,5 +8,11 @@ def main(n):
     Returns:
         bool: answer
     """
+    b=n%100000//10000
+    c=n%10000//1000
+    d=n%1000//100
+    e=n%100//10
+    f=n%10
 
-    return
+    return b+c+d+e+f>5-(b+c+d+e+f)
+print(main(11001))
